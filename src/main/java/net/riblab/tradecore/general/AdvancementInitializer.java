18772dc3b6973c11a5872adb5167f37a2df1a459
くぁーりぃ <kaito6343@gmail.com>
2023-08-31 15:22:57 +0900

    実績本体をenumにして可読性向上

--- src/main/java/net/riblab/tradecore/general/AdvancementInitializer.java
+++ src/main/java/net/riblab/tradecore/general/AdvancementInitializer.java
@@ -25,56 +25,27 @@ import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
 public enum AdvancementInitializer {
     INSTANCE;
 
-    private AdvancementTab primitiveAgeTab;
-
+    public static final AdvancementTab primitiveAgeTab = TradeCore.getInstance().getAdvancementAPI().createAdvancementTab("primitive_age");
+    
     /**
      * 実績が既に登録されたかどうか
      */
     boolean isInit = false;
 
-    private UltimateAdvancementAPI getAPI() {
-        return TradeCore.getInstance().getAdvancementAPI();
-    }
-
     public void init() {
         if (isInit)
             return;
 
-        getAPI().disableVanillaAdvancements();
+        TradeCore.getInstance().getAdvancementAPI().disableVanillaAdvancements();
 
-        primitiveAgeTab = getAPI().createAdvancementTab("primitive_age");
-        
-        RootAdvancement gatherPebbleAdv = new RootAdvancement(primitiveAgeTab, "adv_gather_pebble", AdvancementDisplays.GATHERPEBBLES.get(), "textures/block/stone.png", 1);
-        
-        BaseAdvancement craftHatchetAdv = new BaseAdvancement("adv_craft_hatchet", AdvancementDisplays.CRAFTHATCHETS.get(), gatherPebbleAdv, 1);
-        
-        BaseAdvancement craftTableAdv = new BaseAdvancement("adv_craft_table", AdvancementDisplays.CRAFTTABLE.get(), craftHatchetAdv, 1);
-        
-        BaseAdvancement woodenAxeAdv = new BaseAdvancement("adv_wooden_axe", AdvancementDisplays.WOODENAXE.get(), craftTableAdv, 1);
-        
-        MultiTasksAdvancement woodenComponentAdv = new MultiTasksAdvancement("adv_wooden_component", AdvancementDisplays.WOODENCOMPONENT.get(), woodenAxeAdv, 3);
+        ((MultiTasksAdvancement)Advancements.WOODENCOMPONENT.get()).registerTasks((TaskAdvancement) Advancements.WOODENCOMPONENT_SUB1.get(),(TaskAdvancement) Advancements.WOODENCOMPONENT_SUB2.get(),
+                (TaskAdvancement) Advancements.WOODENCOMPONENT_SUB3.get());
 
-        TaskAdvancement woodpulpTask = new TaskAdvancement("task_woodpulp", woodenComponentAdv);
-        TaskAdvancement dustTask = new TaskAdvancement("task_dust", woodenComponentAdv);
-        TaskAdvancement mossTask = new TaskAdvancement("task_moss", woodenComponentAdv);
+        ((MultiTasksAdvancement)Advancements.STONECOMPONENT.get()).registerTasks((TaskAdvancement) Advancements.STONECOMPONENT_SUB1.get(), (TaskAdvancement) Advancements.STONECOMPONENT_SUB2.get(), 
+                (TaskAdvancement) Advancements.STONECOMPONENT_SUB3.get(), (TaskAdvancement) Advancements.STONECOMPONENT_SUB4.get());
 
-        woodenComponentAdv.registerTasks(woodpulpTask, dustTask, mossTask);
-        
-        BaseAdvancement stoneAxeAdv = new BaseAdvancement("adv_stone_axe", AdvancementDisplays.STONEAXE.get(), woodenComponentAdv, 1);
-        
-        MultiTasksAdvancement stoneComponentAdv = new MultiTasksAdvancement("adv_stone_component", AdvancementDisplays.STONECOMPONENT.get(), stoneAxeAdv, 4);
-
-        TaskAdvancement andesiteStoneTask = new TaskAdvancement("task_andestitestone", stoneComponentAdv);
-        TaskAdvancement graniteStoneTask = new TaskAdvancement("task_granitestone", stoneComponentAdv);
-        TaskAdvancement dioriteStoneTask = new TaskAdvancement("task_dioritestone", stoneComponentAdv);
-        TaskAdvancement roundStoneTask = new TaskAdvancement("task_roundstone", stoneComponentAdv);
-
-        stoneComponentAdv.registerTasks(andesiteStoneTask, graniteStoneTask, dioriteStoneTask, roundStoneTask);
-
-        
-        BaseAdvancement ironAxeAdv = new BaseAdvancement("adv_iron_axe", AdvancementDisplays.IRONAXE.get(), stoneComponentAdv, 1);
-
-        primitiveAgeTab.registerAdvancements(gatherPebbleAdv, craftHatchetAdv, craftTableAdv, woodenAxeAdv, woodenComponentAdv, stoneAxeAdv, stoneComponentAdv, ironAxeAdv);
+        primitiveAgeTab.registerAdvancements((RootAdvancement) Advancements.GATHERPEBBLE.get(), (BaseAdvancement) Advancements.CRAFTHATCHET.get(),(BaseAdvancement) Advancements.CRAFTTABLE.get(),
+                (BaseAdvancement) Advancements.WOODENAXE.get(), (BaseAdvancement) Advancements.WOODENCOMPONENT.get(), (BaseAdvancement) Advancements.STONEAXE.get(), (BaseAdvancement) Advancements.STONECOMPONENT.get(), (BaseAdvancement) Advancements.IRONAXE.get());
         primitiveAgeTab.automaticallyShowToPlayers();
 
         //TODO:プレイヤー単位で実績を管理。ロード時にプレイヤーが未取得の実績を洗い出してそこにだけイベントが飛ぶようにする
@@ -84,14 +55,14 @@ public enum AdvancementInitializer {
                 return;
 
             Player player = playerPickItemEvent.getPlayer();
-            check(itcItem, TCItems.PEBBLE, gatherPebbleAdv, player);
-            check(itcItem, TCItems.WOODPULP, woodpulpTask, player);
-            check(itcItem, TCItems.DUST, dustTask, player);
-            check(itcItem, TCItems.MOSS, mossTask, player);
-            check(itcItem, TCItems.ANDESITE_STONE, andesiteStoneTask, player);
-            check(itcItem, TCItems.GRANITE_STONE, graniteStoneTask, player);
-            check(itcItem, TCItems.DIORITE_STONE, dioriteStoneTask, player);
-            check(itcItem, TCItems.ROUND_STONE, roundStoneTask, player);
+            check(itcItem, TCItems.PEBBLE, Advancements.GATHERPEBBLE.get(), player);
+            check(itcItem, TCItems.WOODPULP, Advancements.WOODENCOMPONENT_SUB1.get(), player);
+            check(itcItem, TCItems.DUST, Advancements.WOODENCOMPONENT_SUB2.get(), player);
+            check(itcItem, TCItems.MOSS, Advancements.WOODENCOMPONENT_SUB3.get(), player);
+            check(itcItem, TCItems.ANDESITE_STONE, Advancements.STONECOMPONENT_SUB1.get(), player);
+            check(itcItem, TCItems.GRANITE_STONE, Advancements.STONECOMPONENT_SUB2.get(), player);
+            check(itcItem, TCItems.DIORITE_STONE, Advancements.STONECOMPONENT_SUB3.get(), player);
+            check(itcItem, TCItems.ROUND_STONE, Advancements.STONECOMPONENT_SUB4.get(), player);
 
         });
 
@@ -100,14 +71,14 @@ public enum AdvancementInitializer {
                 return;
 
             if (event.getRecipe().getResult().getType() == Material.CRAFTING_TABLE) {
-                craftTableAdv.incrementProgression(player);
+                Advancements.CRAFTTABLE.get().incrementProgression(player);
             }
 
             ITCItem itcItem = TCItems.toTCItem(event.getRecipe().getResult());
             if (itcItem == null)
                 return;
 
-            check(itcItem, TCItems.HATCHET, craftHatchetAdv, player);
+            check(itcItem, TCItems.HATCHET, Advancements.CRAFTHATCHET.get(), player);
         });
 
         primitiveAgeTab.getEventManager().register(primitiveAgeTab, BlockBreakEvent.class, event -> {
@@ -116,9 +87,10 @@ public enum AdvancementInitializer {
                 return;
 
             Player player = event.getPlayer();
-            check(itcItem, TCItems.WOODEN_AXE, woodenAxeAdv, player);
-            check(itcItem, TCItems.STONE_AXE, stoneAxeAdv, player);
-            check(itcItem, TCItems.IRON_AXE, ironAxeAdv, player);
+            check(itcItem, TCItems.WOODEN_AXE, Advancements.WOODENAXE.get(), player);
+            check(itcItem, TCItems.STONE_AXE, Advancements.STONEAXE.get(), player);
+            check(itcItem, TCItems.IRON_AXE, Advancements.IRONAXE.get(), player);
+            check(itcItem, TCItems.GOLDEN_AXE, Advancements.IRONAXE.get(), player);
         });
 
         Bukkit.getOnlinePlayers().forEach(player -> primitiveAgeTab.showTab(player));
---
+++ src/main/java/net/riblab/tradecore/general/Advancements.java
@@ -0,0 +1,39 @@
+package net.riblab.tradecore.general;
+
+import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
+import com.fren_gor.ultimateAdvancementAPI.advancement.RootAdvancement;
+import com.fren_gor.ultimateAdvancementAPI.advancement.tasks.AbstractMultiTasksAdvancement;
+import com.fren_gor.ultimateAdvancementAPI.advancement.tasks.MultiTasksAdvancement;
+import com.fren_gor.ultimateAdvancementAPI.advancement.tasks.TaskAdvancement;
+
+import static net.riblab.tradecore.general.AdvancementInitializer.primitiveAgeTab;
+
+public enum Advancements {
+    GATHERPEBBLE(new RootAdvancement(primitiveAgeTab, "adv_gather_pebble", AdvancementDisplays.GATHERPEBBLES.get(), "textures/block/stone.png", 1)),
+    CRAFTHATCHET(new BaseAdvancement("adv_craft_hatchet", AdvancementDisplays.CRAFTHATCHETS.get(), GATHERPEBBLE.get(), 1)),
+    CRAFTTABLE(new BaseAdvancement("adv_craft_table", AdvancementDisplays.CRAFTTABLE.get(), CRAFTHATCHET.get(), 1)),
+    WOODENAXE(new BaseAdvancement("adv_wooden_axe", AdvancementDisplays.WOODENAXE.get(), CRAFTTABLE.get(), 1)),
+    WOODENCOMPONENT(new MultiTasksAdvancement("adv_wooden_component", AdvancementDisplays.WOODENCOMPONENT.get(), WOODENAXE.get(), 3)),
+    WOODENCOMPONENT_SUB1(new TaskAdvancement("task_woodpulp", (AbstractMultiTasksAdvancement) WOODENCOMPONENT.get())),
+    WOODENCOMPONENT_SUB2(new TaskAdvancement("task_dust", (AbstractMultiTasksAdvancement) WOODENCOMPONENT.get())),
+    WOODENCOMPONENT_SUB3(new TaskAdvancement("task_moss", (AbstractMultiTasksAdvancement) WOODENCOMPONENT.get())),
+    STONEAXE(new BaseAdvancement("adv_stone_axe", AdvancementDisplays.STONEAXE.get(), WOODENCOMPONENT.get(), 1)),
+    STONECOMPONENT(new MultiTasksAdvancement("adv_stone_component", AdvancementDisplays.STONECOMPONENT.get(), STONEAXE.get(), 4)),
+    STONECOMPONENT_SUB1(new TaskAdvancement("task_andestitestone", (AbstractMultiTasksAdvancement) STONECOMPONENT.get())),
+    STONECOMPONENT_SUB2(new TaskAdvancement("task_granitestone", (AbstractMultiTasksAdvancement) STONECOMPONENT.get())),
+    STONECOMPONENT_SUB3(new TaskAdvancement("task_dioritestone", (AbstractMultiTasksAdvancement) STONECOMPONENT.get())),
+    STONECOMPONENT_SUB4(new TaskAdvancement("task_roundstone", (AbstractMultiTasksAdvancement) STONECOMPONENT.get())),
+    IRONAXE(new BaseAdvancement("adv_iron_axe", AdvancementDisplays.IRONAXE.get(), STONECOMPONENT.get(), 1))
+    ;
+
+    private final Advancement adv;
+
+    Advancements(Advancement adv) {
+        this.adv = adv;
+    }
+    
+    public Advancement get(){
+        return adv;
+    }
+}
