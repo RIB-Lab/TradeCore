bb9444fcf927b2bc5e9433c77f35b02408ebe5e8
くぁーりぃ <kaito6343@gmail.com>
2023-09-02 02:35:51 +0900

    本番環境でバニラのクラフトレシピが有効化されていたのを修正

--- src/main/java/net/riblab/tradecore/craft/VanillaCraftInitializer.java
+++ src/main/java/net/riblab/tradecore/craft/VanillaCraftInitializer.java
@@ -7,6 +7,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  * バニラレシピの削除・登録クラス
@@ -15,19 +16,25 @@ public enum VanillaCraftInitializer {
     INSTANCE;
 
     public void init(JavaPlugin plugin) {
-        Bukkit.clearRecipes();
+        
+        //他のプラグインがレシピの削除を妨害してくるのでロード後に上書きする
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                Bukkit.clearRecipes();
+                ItemStack hatchet = TCItems.HATCHET.get().getTemplateItemStack();
+                ShapelessRecipe hatchetRecipe = new ShapelessRecipe(new NamespacedKey(plugin, TCItems.HATCHET.get().getInternalName()), hatchet);
+                hatchetRecipe.addIngredient(TCItems.PEBBLE.get().getTemplateItemStack());
+                hatchetRecipe.addIngredient(TCItems.STICK.get().getTemplateItemStack());
+                Bukkit.getServer().addRecipe(hatchetRecipe);
 
-        ItemStack hatchet = TCItems.HATCHET.get().getTemplateItemStack();
-        ShapelessRecipe hatchetRecipe = new ShapelessRecipe(new NamespacedKey(plugin, TCItems.HATCHET.get().getInternalName()), hatchet);
-        hatchetRecipe.addIngredient(TCItems.PEBBLE.get().getTemplateItemStack());
-        hatchetRecipe.addIngredient(TCItems.STICK.get().getTemplateItemStack());
-        Bukkit.getServer().addRecipe(hatchetRecipe);
-
-        ItemStack crafting_table = new ItemStack(Material.CRAFTING_TABLE);
-        ShapelessRecipe crafting_table_recipe = new ShapelessRecipe(new NamespacedKey(plugin, crafting_table.getType().toString().toLowerCase()), crafting_table);
-        ItemStack ingredient = TCItems.ROUND_TRUNK.get().getTemplateItemStack();
-        ingredient.setAmount(4);
-        crafting_table_recipe.addIngredient(ingredient);
-        Bukkit.getServer().addRecipe(crafting_table_recipe);
+                ItemStack crafting_table = new ItemStack(Material.CRAFTING_TABLE);
+                ShapelessRecipe crafting_table_recipe = new ShapelessRecipe(new NamespacedKey(plugin, crafting_table.getType().toString().toLowerCase()), crafting_table);
+                ItemStack ingredient = TCItems.ROUND_TRUNK.get().getTemplateItemStack();
+                ingredient.setAmount(4);
+                crafting_table_recipe.addIngredient(ingredient);
+                Bukkit.getServer().addRecipe(crafting_table_recipe);
+            }
+        }.runTaskLater(plugin, 1);
     }
 }
