ed8d3e0832151fb06d9c11a30111c9ba2e2eb69c
くぁーりぃ <kaito6343@gmail.com>
2023-09-10 13:23:05 +0900

    loottableがmaterialsetの文字列を引数にとるように

--- src/main/java/net/riblab/tradecore/item/LootTable.java
+++ src/main/java/net/riblab/tradecore/item/LootTable.java
@@ -3,22 +3,19 @@
  */
 package net.riblab.tradecore.item;
 
-import net.riblab.tradecore.item.base.ITCItem;
 import net.riblab.tradecore.modifier.IToolStatsModifier;
-import org.bukkit.Material;
 
 import java.util.Map;
-import java.util.Set;
 
 /**
  * バニラのルートテーブルとは別の概念
  *
- * @param material      ドロップテーブルの対象となるバニラのブロックの種類
+ * @param materialSetKey      ドロップテーブルの対象となるバニラのブロックの種類(マテリアルセット)
  * @param toolType      ドロップテーブルを発動させるのに必要なツールの種類
  * @param harvestLevel  ドロップテーブルを発動させるために必要な最小のツールレベル
  * @param dropChanceMap ドロップ率(0~1)とその確率でドロップするアイテムのマップ
  */
-public record LootTable(Set<Material> material,
+public record LootTable(String materialSetKey,
                         IToolStatsModifier.ToolType toolType,
                         int harvestLevel,
                         Map<Float, String> dropChanceMap) {
--- src/main/java/net/riblab/tradecore/item/LootTables.java
+++ src/main/java/net/riblab/tradecore/item/LootTables.java
@@ -15,24 +15,24 @@ import java.util.*;
  * ブロックを壊したとき落ちるアイテムを記述したルートテーブルの一覧
  */
 public enum LootTables {
-    GRASS(new LootTable(Set.of(Material.GRASS, Material.TALL_GRASS), IToolStatsModifier.ToolType.HAND, 0, Map.of(1f, "drygrass", 0.2f, "pebble"))),
-    LEAVES(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("leaves").orElseThrow(), IToolStatsModifier.ToolType.AXE, 0, Map.of(0.2f, "stick"))),
-    LEAVES2(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("leaves").orElseThrow(), IToolStatsModifier.ToolType.AXE, 1, Map.of(0.1f, "stick"))),
-    LOGS(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("logs").orElseThrow(), IToolStatsModifier.ToolType.AXE, 1, Map.of(0.2f, "round_trunk", 0.4f, "bark", 0.8f, "twig"))),
-    LOGS2(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("logs").orElseThrow(), IToolStatsModifier.ToolType.AXE, 2, Map.of(0.2f, "round_trunk"))),
-    LOGS3(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("logs").orElseThrow(), IToolStatsModifier.ToolType.AXE, 3, Map.of(0.1f, "round_trunk"))),
-    DIRTS(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("dirts").orElseThrow(), IToolStatsModifier.ToolType.SHOVEL, 0, Map.of(0.2f, "dust", 0.1f, "mud"))),
-    DIRTS2(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("dirts").orElseThrow(), IToolStatsModifier.ToolType.SHOVEL, 1, Map.of(0.1f, "dust", 0.05f, "mud"))),
-    PRIMITIVE_STONES(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("primitive_stones").orElseThrow(), IToolStatsModifier.ToolType.PICKAXE, 0, Map.of(0.2f, "widestone", 0.21f, "tallstone"))),
-    PRIMITIVE_STONES2(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("primitive_stones").orElseThrow(), IToolStatsModifier.ToolType.PICKAXE, 1, Map.of(0.1f, "round_stone"))),
-    PRIMITIVE_PLANKS(new LootTable(MaterialSetRegistry.INSTANCE.commandToMaterialSet("primitive_stones").orElseThrow(), IToolStatsModifier.ToolType.AXE, 2, Map.of(1.0f, "woodpulp"))),
-    MOSS(new LootTable(Set.of(Material.FARMLAND), IToolStatsModifier.ToolType.HOE, 0, Map.of(0.2f, "moss", 1.0f, "drygrass"))),
-    MOSS2(new LootTable(Set.of(Material.FARMLAND), IToolStatsModifier.ToolType.HOE, 0, Map.of(0.1f, "moss", 0.5f, "drygrass"))),
-    ANDESITE(new LootTable(Set.of(Material.ANDESITE), IToolStatsModifier.ToolType.PICKAXE, 1, Map.of(0.2f, "andesite_stone"))),
-    GRANITE(new LootTable(Set.of(Material.GRANITE), IToolStatsModifier.ToolType.PICKAXE, 1, Map.of(0.2f, "granite_stone"))),
-    DIORITE(new LootTable(Set.of(Material.DIORITE), IToolStatsModifier.ToolType.PICKAXE, 1, Map.of(0.2f, "diorite_stone"))),
-    GRAVEL(new LootTable(Set.of(Material.GRAVEL), IToolStatsModifier.ToolType.SHOVEL, 1, Map.of(0.2f, "gravel_dust", 0.02f, "flint", 0.021f, "meteoric_iron_ore"))),
-    SAND(new LootTable(Set.of(Material.SAND), IToolStatsModifier.ToolType.SHOVEL, 1, Map.of(0.2f, "sand_dust", 0.02f, "sandgold")));
+    GRASS(new LootTable("grasses", IToolStatsModifier.ToolType.HAND, 0, Map.of(1f, "drygrass", 0.2f, "pebble"))),
+    LEAVES(new LootTable("leaves", IToolStatsModifier.ToolType.AXE, 0, Map.of(0.2f, "stick"))),
+    LEAVES2(new LootTable("leaves", IToolStatsModifier.ToolType.AXE, 1, Map.of(0.1f, "stick"))),
+    LOGS(new LootTable("logs", IToolStatsModifier.ToolType.AXE, 1, Map.of(0.2f, "round_trunk", 0.4f, "bark", 0.8f, "twig"))),
+    LOGS2(new LootTable("logs", IToolStatsModifier.ToolType.AXE, 2, Map.of(0.2f, "round_trunk"))),
+    LOGS3(new LootTable("logs", IToolStatsModifier.ToolType.AXE, 3, Map.of(0.1f, "round_trunk"))),
+    DIRTS(new LootTable("dirts", IToolStatsModifier.ToolType.SHOVEL, 0, Map.of(0.2f, "dust", 0.1f, "mud"))),
+    DIRTS2(new LootTable("dirts", IToolStatsModifier.ToolType.SHOVEL, 1, Map.of(0.1f, "dust", 0.05f, "mud"))),
+    PRIMITIVE_STONES(new LootTable("primitive_stones", IToolStatsModifier.ToolType.PICKAXE, 0, Map.of(0.2f, "widestone", 0.21f, "tallstone"))),
+    PRIMITIVE_STONES2(new LootTable("primitive_stones", IToolStatsModifier.ToolType.PICKAXE, 1, Map.of(0.1f, "round_stone"))),
+    PRIMITIVE_PLANKS(new LootTable("primitive_stones", IToolStatsModifier.ToolType.AXE, 2, Map.of(1.0f, "woodpulp"))),
+    MOSS(new LootTable("farmland", IToolStatsModifier.ToolType.HOE, 0, Map.of(0.2f, "moss", 1.0f, "drygrass"))),
+    MOSS2(new LootTable("farmland", IToolStatsModifier.ToolType.HOE, 0, Map.of(0.1f, "moss", 0.5f, "drygrass"))),
+    ANDESITE(new LootTable("andesite", IToolStatsModifier.ToolType.PICKAXE, 1, Map.of(0.2f, "andesite_stone"))),
+    GRANITE(new LootTable("granite", IToolStatsModifier.ToolType.PICKAXE, 1, Map.of(0.2f, "granite_stone"))),
+    DIORITE(new LootTable("diorite", IToolStatsModifier.ToolType.PICKAXE, 1, Map.of(0.2f, "diorite_stone"))),
+    GRAVEL(new LootTable("gravel", IToolStatsModifier.ToolType.SHOVEL, 1, Map.of(0.2f, "gravel_dust", 0.02f, "flint", 0.021f, "meteoric_iron_ore"))),
+    SAND(new LootTable("sand", IToolStatsModifier.ToolType.SHOVEL, 1, Map.of(0.2f, "sand_dust", 0.02f, "sandgold")));
 
     private final LootTable table;
 
@@ -54,7 +54,7 @@ public enum LootTables {
         Multimap<Float, String> itemMap = ArrayListMultimap.create();
         List<Map<Float, String>> itemMaps = Arrays.stream(LootTables.values())
                 .map(LootTables::get)
-                .filter(table1 -> table1.material().contains(material))
+                .filter(table1 -> MaterialSetRegistry.INSTANCE.commandToMaterialSet(table1.materialSetKey()).orElseThrow().contains(material))
                 .filter(table1 -> table1.toolType() == toolType)
                 .map(LootTable::dropChanceMap).toList();
         itemMaps.forEach(floatITCItemMap -> floatITCItemMap.forEach(itemMap::put));
@@ -72,7 +72,7 @@ public enum LootTables {
         Multimap<Float, String> itemMultiMap = ArrayListMultimap.create();
         List<Map<Float, String>> itemMaps = Arrays.stream(LootTables.values())
                 .map(LootTables::get)
-                .filter(table1 -> table1.material().contains(material))
+                .filter(table1 -> MaterialSetRegistry.INSTANCE.commandToMaterialSet(table1.materialSetKey()).orElseThrow().contains(material))
                 .filter(table1 -> table1.toolType() == toolStats.getToolType())
                 .filter(lootTable -> lootTable.harvestLevel() <= toolStats.getHarvestLevel())
                 .map(LootTable::dropChanceMap).toList();
@@ -88,7 +88,7 @@ public enum LootTables {
         IToolStatsModifier.ToolStats toolStats = toolStatsMod.apply(null, null);
         List<Integer> hardnessList = Arrays.stream(LootTables.values())
                 .map(LootTables::get)
-                .filter(table1 -> table1.material().contains(material))
+                .filter(table1 -> MaterialSetRegistry.INSTANCE.commandToMaterialSet(table1.materialSetKey()).orElseThrow().contains(material))
                 .filter(table1 -> table1.toolType() == toolStats.getToolType())
                 .map(LootTable::harvestLevel).toList();
         if (hardnessList.isEmpty())
